936f71d79ce982f3eeba8ef31fea534223f0c78b
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-10-28 13:39:10 +0100

    populates books table

---
+++ 01/db/03-insert-books.sql
@@ -0,0 +1,16 @@
+\c treat_your_shelf;
+
+INSERT INTO books (title, price_in_pence, quantity_in_stock, release_date, is_fiction)
+VALUES 
+    ('The Hitchhiker''s Guide to the Galaxy', 599, 100, '1979-10-12', true),
+    ('The Restaurant at the End of the Universe', 599, 100, '1980-10-12', true),
+    ('The Little Prince', 699, 1020, '1943-04-06', true),
+    ('The Tale of Peter Rabbit', 599, 1000, '1902-10-01', true),
+    ('Emma', 522, 390, 	'1815-12-23', true),
+    ('Nineteen Eighty-Four: A Novel', 799, 420, '1949-06-08', true),
+    ('The Handmaid''s Tale', 899, 10, '08-01-1985' ,true),
+    ('The War of the Worlds', 250, 17, '04-01-1897', true),
+    ('Captain Corelli''s Mandolin', 999, 0, '08-29-1995', true),
+    ('A Brief History of Time', 825, 0, '04-01-1988', false),
+    ('Pride and Prejudice', 699, 4, '01-28-1813', true);
+
