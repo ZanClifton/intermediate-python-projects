385cffb6f6d0b4bbca5b767527583e2085efb036
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-11-05 12:14:02 +0000

    added additional books

--- 01/db/10-update-books-author.sql
+++ 01/db/10-update-books-author.sql
@@ -2,15 +2,36 @@
 
 UPDATE books
 SET author_id=CASE
-    WHEN title='The Hitchhiker''s Guide to the Galaxy' THEN 3
-    WHEN title='The Little Prince' THEN 2
-    WHEN title='The Tale of Peter Rabbit' THEN 7
-    WHEN title='Emma' THEN 14
-    WHEN title='Nineteen Eighty-Four: A Novel' THEN 13
-    WHEN title='The Handmaid''s Tale' THEN 15
-    WHEN title='The War of the Worlds' THEN 12
-    WHEN title='Pride and Prejudice' THEN 14
-    WHEN title='The Restaurant at the End of the Universe' THEN 3
+    WHEN id=1 THEN 3
+    WHEN id=3 THEN 2
+    WHEN id=4 THEN 7
+    WHEN id=5 THEN 14
+    WHEN id=6 THEN 13
+    WHEN id=7 THEN 15
+    WHEN id=8 THEN 12
+    WHEN id=11 THEN 14
+    WHEN id=2 THEN 3
+    WHEN id=12 THEN 9
+    WHEN id=13 THEN 9 
+    WHEN id=14 THEN 9
+    WHEN id=15 THEN 9
+    WHEN id=16 THEN 9 
+    WHEN id=17 THEN 9
+    WHEN id=18 THEN 9
+    WHEN id=19 THEN 9 
+    WHEN id=20 THEN 9 
+    WHEN id=21 THEN 9
+    WHEN id=22 THEN 9
+    WHEN id=23 THEN 9
+    WHEN id=24 THEN 9
+    WHEN id=25 THEN 9
+    WHEN id=26 THEN 3
+    WHEN id=27 THEN 3
+    WHEN id=28 THEN 3
+    WHEN id=29 THEN 3
+    WHEN id=30 THEN 3
+    WHEN id=31 THEN 3
+    WHEN id=32 THEN 3
 END;
 
 ALTER TABLE books
