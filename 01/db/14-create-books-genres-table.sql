a4874c842939949c1a8ec9f5dd69e1716025a606
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-11-26 14:11:05 +0000

    adds on delete cascade constraints

--- 01/db/14-create-books-genres-table.sql
+++ 01/db/14-create-books-genres-table.sql
@@ -2,8 +2,6 @@
 
 CREATE TABLE books_genres (
     id SERIAL PRIMARY KEY,
-    book_id INTEGER REFERENCES books(id),
-    genre_id INTEGER REFERENCES genres(id)
+    book_id INTEGER REFERENCES books(id) ON DELETE CASCADE,
+    genre_id INTEGER REFERENCES genres(id) ON DELETE CASCADE
 );
-
-SELECT * FROM books_genres;
