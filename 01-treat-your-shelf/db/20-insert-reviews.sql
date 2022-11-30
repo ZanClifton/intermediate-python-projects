0cfec6db090b33d228029197efad6e452fff7198
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-11-30 09:06:58 +0000

    inserts some reviews

---
+++ 01-treat-your-shelf/db/20-insert-reviews.sql
@@ -0,0 +1,13 @@
+\c treat_your_shelf;
+
+INSERT INTO reviews (
+    book_id,
+    review_title,
+    review_body,
+    rating
+)
+VALUES
+    (7, 'Hard going', 'No doubt this is a classic for a reason, but this was one of the hardest books to read. I couldn''t finish it; it was so bleak. Brilliantly written, but too close to the kuckle and made me very uncomfortable.', 5),
+    (7, 'English', 'As an English literature student I required this book for my studies, it is a good copy of the book, easy to analyse', 4),
+    (7, 'Bought for my son', 'Bought this for my son so can''t review it properly although he said it was brilliant', 5),
+    (7, 'Fantastic read! Loved it.', 'One of the best books I have read in a while. I don''t usually like fiction, but this is something else. Fantastic read!', 5);
