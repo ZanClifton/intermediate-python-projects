100495633e67f344fed2a9e1d6456a4eaf6b6559
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-12-01 09:14:12 +0000

    adds new reviews and queries to reviews table

--- 01-treat-your-shelf/db/21-query-reviews.sql
+++ 01-treat-your-shelf/db/21-query-reviews.sql
@@ -3,3 +3,24 @@
 \echo '\n----- all available reviews -----'
 
 SELECT * FROM reviews;
+
+\echo '\n----- all reviews of The Handmaid'' Tale -----'
+
+SELECT books.title, reviews.rating, reviews.review_title, reviews.review_body FROM reviews
+JOIN books ON books.id = reviews.book_id
+WHERE books.title = 'The Handmaid''s Tale';
+
+\echo '\n----- the average rating of each book -----'
+
+SELECT book_id, ROUND(AVG(rating), 2) AS average_rating 
+FROM reviews
+GROUP BY book_id;
+
+\echo '\n----- the average rating of The Handmaid''s Tale -----'
+
+SELECT books.title, ROUND(AVG(reviews.rating), 2) AS average_rating 
+FROM reviews
+JOIN books ON books.id = reviews.book_id
+WHERE book_id = 7
+GROUP BY books.title;
+
