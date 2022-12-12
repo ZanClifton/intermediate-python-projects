279e38a419465abe62c94276aac7290460ab5136
Zan Clifton <ZanClifton@users.noreply.github.com>
2022-12-12 21:21:07 +0000

    refactors snake into separate class and file

--- 02-snake/main.py
+++ 02-snake/main.py
@@ -1,34 +1,24 @@
 from turtle import Screen, Turtle
 import time
 
+from snake import Snake
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snek")
 screen.tracer(0)
 
-segments = []
-
-for segment_index in range(0, 3):
-    segment = Turtle(shape="square")
-    segment.penup()
-    segment.color("pale green")
-    segment.goto(x=0 - 20 * segment_index, y=0)
-    segments.append(segment)
-
 screen.update()
+
+snek = Snake()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(1)
+    time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):  # start, stop, step
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
+    snek.move()
 
 screen.exitonclick()
