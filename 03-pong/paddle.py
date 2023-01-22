b7205cd062b41a4e203c4fc26c2889c81cd3e054
Zan Clifton <ZanClifton@users.noreply.github.com>
2023-01-22 15:58:20 +0000

    creates screen and paddles

---
+++ 03-pong/paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+
+class Paddle(Turtle):
+
+    def __init__(self, start_pos):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1)
+        self.penup()
+        self.goto(start_pos, 0)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
