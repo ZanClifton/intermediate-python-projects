41b9bdf4b9bbda635740410ed512eeaa654d7533
Zan Clifton <ZanClifton@users.noreply.github.com>
2023-01-28 13:47:53 +0000

    player returns to start on win

--- 04-turtle-crossing/turtle-crossing-start/main.py
+++ 04-turtle-crossing/turtle-crossing-start/main.py
@@ -37,4 +37,7 @@ while game_is_on:
         if player.distance(car) < 15:
             game_is_on = False
 
+    if player.is_at_finish_line():
+        player.go_to_start()
+
 screen.exitonclick()
--- 04-turtle-crossing/turtle-crossing-start/player.py
+++ 04-turtle-crossing/turtle-crossing-start/player.py
@@ -4,9 +4,6 @@ STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-# starts at the bottom of the screen
-# moves up only - cannot move left, right or back
-
 
 class Player(Turtle):
 
@@ -21,3 +18,12 @@ class Player(Turtle):
     def move(self):
         new_y = self.ycor() + MOVE_DISTANCE
         self.goto(self.xcor(), new_y)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        if self.ycor() == FINISH_LINE_Y:
+            return True
+        else:
+            return False
