9d5afec4f3bd10f2ca25d0cb4207922ed5e6b234
Zan Clifton <ZanClifton@users.noreply.github.com>
2023-01-28 13:10:47 +0000

    adds stop game when turtle collides with car

--- 04-turtle-crossing/turtle-crossing-start/main.py
+++ 04-turtle-crossing/turtle-crossing-start/main.py
@@ -10,13 +10,31 @@ screen.tracer(0)
 
 player = Player()
 
+cars = []
+
+for i in range(1):
+    car = CarManager()
+    cars.append(car)
+
 screen.listen()
 screen.onkey(player.move, "Up")
+
+count = 0
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    count += 1
+
+    if count % 8 == 0:
+        car = CarManager()
+        cars.append(car)
+
+    for car in cars:
+        car.move()
+        if player.distance(car) < 15:
+            game_is_on = False
 
 screen.exitonclick()
