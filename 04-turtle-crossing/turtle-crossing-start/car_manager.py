b3cc76cd3f57d504d799b2180a03b0d25793de6e
Zan Clifton <ZanClifton@users.noreply.github.com>
2023-01-28 13:02:55 +0000

    adds moving cars

---
+++ 04-turtle-crossing/turtle-crossing-start/car_manager.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+from random import choice, randint
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.shapesize(1, 1.8)
+        self.color(choice(COLORS))
+        self.penup()
+        self.goto(300, randint(-10, 10) * 20)
+
+    def move(self):
+        new_x = self.xcor() - STARTING_MOVE_DISTANCE
+        self.goto(new_x, self.ycor())
