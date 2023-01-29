6448ca95b86659b06e41af19c8ad0522992346f6
Zan Clifton <ZanClifton@users.noreply.github.com>
2023-01-29 15:57:55 +0000

    snek

--- 02-snek/scoreboard.py
+++ 02-snek/scoreboard.py
@@ -10,18 +10,20 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.pendown()
-        self.write(f"Score: {self.score}   High Score: {self.high_score}", move=MOVE, align=ALIGNMENT,
-                   font=FONT)
+        self.update_scoreboard()
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
