57eb892aade8d0bd23204d985da85c0b17f0d904
ZanClifton <zan@zanclifton.dev>
2023-05-05 11:24:47 +0100

    language flash cards

--- 11-language-flash-cards/main.py
+++ 11-language-flash-cards/main.py
@@ -3,6 +3,8 @@ from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 
 BACKGROUND_COLOR = "#B1DDC6"
+LANGUAGE_FONT = ("Ariel", 40, "italic")
+WORD_FONT = ("Ariel", 60, "bold")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -14,6 +16,9 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 266, image=card_front)
 canvas.grid(column=0, row=0, columnspan=2)
+
+language = canvas.create_text(400, 150, text="French", font=LANGUAGE_FONT)
+word = canvas.create_text(400, 263, text="word", font=WORD_FONT)
 
 # Buttons
 
