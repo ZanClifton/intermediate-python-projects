3ea8fac67b880582811843ba92d4d7fe36e430f6
zan-clifton-jisc <zan.clifton@jisc.ac.uk>
2023-06-23 12:01:04 +0100

    trivial

--- 18-trivial/quiz_brain.py
+++ 18-trivial/quiz_brain.py
@@ -1,3 +1,5 @@
+import html
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -8,12 +10,13 @@ class QuizBrain:
         return self.question_number < len(self.question_list)
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
+        q_text = html.unescape(self.current_question.text)
         user_answer = input(
-            f"Q.{self.question_number}: {current_question.text} (True/False)?: "
+            f"Q.{self.question_number}: {q_text} (True/False)?: "
         )
-        self.check_answer(user_answer, current_question.answer)
+        self.check_answer(user_answer, self.current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
