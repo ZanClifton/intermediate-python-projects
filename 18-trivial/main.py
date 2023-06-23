c9f77947375476d9b82a93603b95c2199d91434a
zan-clifton-jisc <zan.clifton@jisc.ac.uk>
2023-06-23 11:40:15 +0100

    trivial

--- 18-trivial/main.py
+++ 18-trivial/main.py
@@ -1,3 +1,5 @@
+import re
+
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
@@ -8,7 +10,7 @@ print(logo)
 question_bank = []
 
 for question in question_data:
-    question_text = question["question"]
+    question_text = question["question"].replace("&#039;", "'")
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
